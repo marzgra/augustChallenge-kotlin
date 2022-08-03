7ceba139c4116f96d9346e389843d71c2c573c57
marzgra <marzgra@o2.pl>
2022-08-03 12:43:19 +0200

    Day 2.2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,39 @@
-class Main {
-
-    fun listFunctions() {
-        val list = listOf("Java")
+fun listFunctions() {
+    val list = listOf("Java")
 //    list.add("Kotlin") // cannot edit read-only collection
 
-        val mutableList = mutableListOf("Java")
-        mutableList.add("Kotlin")
+    val mutableList = mutableListOf("Java")
+    mutableList.add("Kotlin")
+}
+
+fun getMax(a: Int, b: Int): Int {
+    return if (a > b) a else b
+}
+
+fun getMaxFunc(a: Int, b: Int) = if (a > b) a else b
+
+
+fun isValidIdentifier(s: String): Boolean {
+    if (s.isBlank() || s[0] in '0'..'9') return false
+
+    fun isValidChar(ch: Char): Boolean =
+        ch in 'a'..'z' ||
+                ch in 'A'..'Z' ||
+                ch in '0'..'9' ||
+                ch == '_'
+
+    for (i in 1 until s.length) {
+        if (!isValidChar(s[i])) return false
     }
 
-    fun getMax(a: Int, b: Int): Int {
-        return if (a > b) a else b
-    }
+    return true
+}
 
-    fun getMaxFunc(a: Int, b: Int) = if (a > b) a else b
-
-    }
+fun main(args: Array<String>) {
+    println(isValidIdentifier("name"))   // true
+    println(isValidIdentifier("_name"))  // true
+    println(isValidIdentifier("_12"))    // true
+    println(isValidIdentifier(""))       // false
+    println(isValidIdentifier("012"))    // false
+    println(isValidIdentifier("no$"))    // false
+}
